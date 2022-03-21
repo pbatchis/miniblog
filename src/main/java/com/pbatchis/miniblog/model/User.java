7281029479495447d42f968598eb4fa76323f2bc
pbatchis <102021513+pbatchis@users.noreply.github.com>
2022-03-22 02:15:31 +0700

    Create User Entity and Repository classes.

---
+++ src/main/java/com/pbatchis/miniblog/model/User.java
@@ -0,0 +1,63 @@
+package com.pbatchis.miniblog.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Version;
+
+/**
+ * A user of the application.
+ * The user data is persisted in the database and used 
+ * for authentication and authorization.
+ * 
+ * The table is named "app_user" so not to conflict with  
+ * the reserved word "user" in PostgreSQL database 
+ * referencing a user of the database management system.
+ */
+@Entity
+@Table(name = "app_user")
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+    @Version
+    private long version;
+
+	@Column(unique = true)
+	private String username;
+
+	private String password;
+
+	public User() {
+	}
+
+	public User(String username, String password) {
+		this.username = username;
+		this.password = password;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
---
+++ src/main/java/com/pbatchis/miniblog/repository/UserRepository.java
@@ -0,0 +1,17 @@
+package com.pbatchis.miniblog.repository;
+
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.pbatchis.miniblog.model.User;
+
+/**
+ * Persistence operations for the User entity.
+ */
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+	Optional<User> findByUsername(String username);
+	
+    Boolean existsByUsername(String username);
+}
