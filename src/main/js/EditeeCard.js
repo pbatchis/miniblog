9c80c361a47027bc54fd0e3464182293ae5e35e4
pbatchis <102021513+pbatchis@users.noreply.github.com>
2022-04-12 21:47:35 +0700

    Refactor EditeeCard to function component.

--- src/main/js/EditeeCard.js
+++ src/main/js/EditeeCard.js
@@ -1,107 +1,102 @@
-import postAuthJson from './postAuthJson';
+import React, { useState } from "react";
+import postAuthJson from "./postAuthJson";
 
-const React = require('react');
-
-class EditeeCard extends React.Component {
+function EditeeCard(props) {
     
-    constructor(props) {
-        super(props);
-        this.state = {
-            category: this.props.category,
-            name: this.props.name,
-            content: this.props.content,
-            status: this.props.status
-        }
-        this.handleChangeCategory = this.handleChangeCategory.bind(this);
-        this.handleChangeName = this.handleChangeName.bind(this);
-        this.handleChangeContent = this.handleChangeContent.bind(this);
-        this.handleChangeStatus = this.handleChangeStatus.bind(this);
-        this.handleEditOk = this.handleEditOk.bind(this);
-        this.handleEditCancel = this.handleEditCancel.bind(this);
+    const [category, setCategory] = useState(props.category);
+    const [name, setName] = useState(props.name);
+    const [content, setContent] = useState(props.content);
+    const [status, setStatus] = useState(props.status);
+
+    function handleChangeCategory(event) {
+        setCategory(event.target.value);
     }
 
-    handleChangeCategory(event) {
-        this.setState({category: event.target.value});
-    }
-    handleChangeName(event) {
-        this.setState({name: event.target.value});
-    }
-    handleChangeContent(event) {
-        this.setState({content: event.target.value});
-    }
-    handleChangeStatus(event) {
-        this.setState({status: event.target.value});
+    function handleChangeName(event) {
+        setName(event.target.value);
     }
 
-    handleEditCancel() {
-        this.props.onEndEdit(this.props.id);
+    function handleChangeContent(event) {
+        setContent(event.target.value);
     }
 
-    handleEditOk() {
-        if (this.props.id === "-1") {
-            // This is a new card being added.
-            postAuthJson('/api/card/add', this.props.jwtToken, {
-                name: this.state.name,
-                status: this.state.status,
-                content: this.state.content,
-                category: this.state.category
-            })
-            .then((response) => {
-                if (response.ok) {
-                    response.json().then(data => {
-                        const id = data.message.substr(5).split(']')[0];
-                        this.props.onEndEditAndAdd(
-                            id,
-                            this.state.name,
-                            this.state.status,
-                            this.state.content,
-                            this.state.category
-                        );
-                    });
-                };
-            });
+    function handleChangeStatus(event) {
+        setStatus(event.target.value);
+    }
+
+    function handleEditCancel() {
+        props.onEndEdit(props.id);
+    }
+
+    function handleEditOk() {
+        if (props.id === "-1") {
+            addCard();
         } else {
-            // This is an existing card being edited.
-            // Save the card state, then end the edit session and update card state.
-            postAuthJson('/api/card/edit', this.props.jwtToken, {
-                id: this.props.id,
-                name: this.state.name,
-                status: this.state.status,
-                content: this.state.content,
-                category: this.state.category
-            })
-            .then((response) => {
-                if (response.ok) {
-                    this.props.onEndEditAndUpdate(
-                        this.props.id,
-                        this.state.name,
-                        this.state.status,
-                        this.state.content,
-                        this.state.category
-                    );
-                }
-            })
+            editCard();
         }
     }
 
-    render() {
-        return (
-            <div className="EditeeCard Card">
-                <div className="row1">
-                    <input className="category textInput" type="text" name="category" placeholder="Category" onChange={this.handleChangeCategory} value={this.state.category} />
-                    <div></div>
-                </div>
-                <input className="name textInput" type="text" name="name" placeholder="Name" onChange={this.handleChangeName} value={this.state.name} />
-                <textarea className="content textInput" name="content" placeholder="Content" onChange={this.handleChangeContent} value={this.state.content} />
-                <input className="status textInput" type="text" name="status" placeholder="Status" onChange={this.handleChangeStatus} value={this.state.status} />
-                <div className="author">{this.props.author}</div>
-                <div className="okCancelButtons">
-                    <button onClick={this.handleEditCancel}>Cancel</button>
-                    <button onClick={this.handleEditOk}>OK</button>
-                </div>
-            </div>
-        )
+    function addCard() {
+        postAuthJson("/api/card/add", props.jwtToken, {
+            name: name,
+            status: status,
+            content: content,
+            category: category
+        })
+        .then( response => {
+            if (response.ok) {
+                response.json().then( data => {
+                    const id = data.message.substr(5).split("]")[0];
+                    props.onEndEditAndAdd(
+                        id,
+                        name,
+                        status,
+                        content,
+                        category
+                    );
+                });
+            }
+        });        
     }
+
+    function editCard() {
+        // Save the card state, then end the edit session and update card state.
+        postAuthJson("/api/card/edit", props.jwtToken, {
+            id: props.id,
+            name: name,
+            status: status,
+            content: content,
+            category: category
+        })
+        .then( response => {
+            if (response.ok) {
+                props.onEndEditAndUpdate(
+                    props.id,
+                    name,
+                    status,
+                    content,
+                    category
+                );
+            }
+        });
+    }
+
+    return (
+        <div className="EditeeCard Card">
+            <div className="row1">
+                <input className="category textInput" type="text" name="category" placeholder="Category" onChange={handleChangeCategory} value={category} />
+                <div></div>
+            </div>
+            <input className="name textInput" type="text" name="name" placeholder="Name" onChange={handleChangeName} value={name} />
+            <textarea className="content textInput" name="content" placeholder="Content" onChange={handleChangeContent} value={content} />
+            <input className="status textInput" type="text" name="status" placeholder="Status" onChange={handleChangeStatus} value={status} />
+            <div className="author">{props.author}</div>
+            <div className="okCancelButtons">
+                <button onClick={handleEditCancel}>Cancel</button>
+                <button onClick={handleEditOk}>OK</button>
+            </div>
+        </div>
+    );
 }
 
-export default EditeeCard
+export default EditeeCard;
