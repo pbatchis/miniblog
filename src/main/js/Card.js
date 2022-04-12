0efdaff7fbd02332714a68191c2f68a130397d3d
pbatchis <102021513+pbatchis@users.noreply.github.com>
2022-04-12 20:48:27 +0700

    Refactoring Card to function component.

--- src/main/js/Card.js
+++ src/main/js/Card.js
@@ -1,66 +1,55 @@
-const React = require('react');
+import React, { useState } from 'react';
 
-class Card extends React.Component {
+function Card(props) {
     
-    constructor(props) {
-        super(props);
-        this.state = {
-            confirmingDelete: false
-        }
-        this.handleEdit = this.handleEdit.bind(this);
-        this.handleDelete = this.handleDelete.bind(this);
-        this.handleCancelDelete = this.handleCancelDelete.bind(this);
-        this.handleConfirmDelete = this.handleConfirmDelete.bind(this);
-    }
+    const [confirmingDelete, setConfirmingDelete] = useState(false);
+    
+    function handleEdit() {
+        props.onEdit(props.id);
+    }    
 
-    handleEdit() {
-        this.props.onEdit(this.props.id);
-    }
+    function handleDelete() {
+        setConfirmingDelete(true);
+    }    
 
-    handleDelete() {
-        this.setState({confirmingDelete: true});
-    }
+    function handleCancelDelete() {
+        setConfirmingDelete(false);
+    }    
 
-    handleCancelDelete() {
-        this.setState({confirmingDelete: false});
-    }
+    function handleConfirmDelete() {
+        props.onDelete(props.id);
+    }    
 
-    handleConfirmDelete() {
-        this.props.onDelete(this.props.id);
-    }
+    function isEditable() {
+        return (props.author === props.signedInUsername);
+    }    
 
-    isEditable() {
-        return (this.props.author === this.props.signedInUsername)
-    }
-
-    render() {
-        return (
-            <div className="Card">
-                {this.state.confirmingDelete &&
-                    <div className="confirmDelete">
-                        <div>Are you sure you want to Delete this card?</div>
-                        <div className="confirmDeleteButtons">
-                            <button onClick={this.handleCancelDelete}>Cancel</button>
-                            <button onClick={this.handleConfirmDelete}>Yes, Delete</button>
-                        </div>
+    return (
+        <div className="Card">
+            {confirmingDelete &&
+                <div className="confirmDelete">
+                    <div>Are you sure you want to Delete this card?</div>
+                    <div className="confirmDeleteButtons">
+                        <button onClick={handleCancelDelete}>Cancel</button>
+                        <button onClick={handleConfirmDelete}>Yes, Delete</button>
+                    </div>
+                </div>
+            }
+            <div className="row1">
+                <div className="category">{props.category}</div>
+                {isEditable() && !confirmingDelete &&
+                    <div>
+                        <button onClick={handleEdit}>Edit</button>
+                        <button onClick={handleDelete}>Delete</button>
                     </div>
                 }
-                <div className="row1">
-                    <div className="category">{this.props.category}</div>
-                    {this.isEditable() && !this.state.confirmingDelete &&
-                        <div>
-                            <button onClick={this.handleEdit}>Edit</button>
-                            <button onClick={this.handleDelete}>Delete</button>
-                        </div>
-                    }
-                </div>
-                <div className="name">{this.props.name}</div>
-                <div className="content">{this.props.content}</div>
-                <div className="status">{this.props.status}</div>
-                <div className="author">{this.props.author}</div>
             </div>
-        )
-    }
+            <div className="name">{props.name}</div>
+            <div className="content">{props.content}</div>
+            <div className="status">{props.status}</div>
+            <div className="author">{props.author}</div>
+        </div>
+    );
 }
 
-export default Card
+export default Card;
