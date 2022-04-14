d1adf8265f08c69bba644a9fd6d05a000de3ce90
pbatchis <102021513+pbatchis@users.noreply.github.com>
2022-04-14 15:44:39 +0700

    Update BlogView.js
    Refactor to function component.

--- src/main/js/BlogView.js
+++ src/main/js/BlogView.js
@@ -1,57 +1,66 @@
-const React = require("react");
-const Card = require("./Card").default;
-const EditeeCard = require("./EditeeCard").default;
-const NewCard = require("./NewCard").default;
-const postAuthJson = require("./postAuthJson").default;
+import React, { useState, useEffect } from "react";
+import Card from "./Card";
+import EditeeCard from "./EditeeCard";
+import NewCard from "./NewCard";
+import postAuthJson from "./postAuthJson";
 
-class BlogView extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      cards: [],
-      editeeId: null, // ID of the card being edited.
-    };
-    this.loadCards = this.loadCards.bind(this);
-    this.handleNewCard = this.handleNewCard.bind(this);
-    this.handleBeginEdit = this.handleBeginEdit.bind(this);
-    this.handleEndEdit = this.handleEndEdit.bind(this);
-    this.handleEndEditAndUpdate = this.handleEndEditAndUpdate.bind(this);
-    this.handleEndEditAndAdd = this.handleEndEditAndAdd.bind(this);
-    this.handleDelete = this.handleDelete.bind(this);
-  }
+function BlogView(props) {
+  const [cards, setCards] = useState([]);
+  const [editeeId, setEditeeId] = useState(null); // ID of the card being edited.
 
-  componentDidMount() {
-    this.loadCards();
-  }
+  useEffect(() => {
+    loadCards();
+  }, []);
 
-  loadCards() {
+  function loadCards() {
     fetch("/api/card/get").then((response) => {
       if (response.ok) {
         response.json().then((data) => {
-          this.setState({
-            // reverse sort cards by id
-            cards: data.cards.sort((a, b) => b.id - a.id),
-          });
+          // reverse sort cards by id
+          setCards(data.cards.sort((a, b) => b.id - a.id));
         });
       }
     });
   }
 
-  handleNewCard() {
-    this.setState((state, props) => {
-      if (!(state.editeeId === null)) {
-        return {};
-      }
-      const updatedCards = [];
+  function handleNewCard() {
+    if (editeeId !== null) {
+      return;
+    }
+    const updatedCards = [];
+    updatedCards.push({
+      id: -1,
+      name: "",
+      status: "",
+      content: "",
+      category: "",
+      author: props.username,
+    });
+    for (let card of cards) {
       updatedCards.push({
-        id: -1,
-        name: "",
-        status: "",
-        content: "",
-        category: "",
-        author: props.username,
+        id: card.id,
+        name: card.name,
+        status: card.status,
+        content: card.content,
+        category: card.category,
+        author: card.author,
       });
-      for (let card of state.cards) {
+    }
+    setCards(updatedCards);
+    setEditeeId("-1");
+  }
+
+  function handleBeginEdit(cardId) {
+    setEditeeId(cardId);
+  }
+
+  function handleEndEdit(cardId) {
+    // remove card from state if it doesn't exist (a cancelled add card).
+    // remove card from editee session.
+    const updatedCards = [];
+    for (let card of cards) {
+      // id of -1 means the card didn't exist.
+      if (!(card.id === -1)) {
         updatedCards.push({
           id: card.id,
           name: card.name,
@@ -61,134 +70,93 @@ class BlogView extends React.Component {
           author: card.author,
         });
       }
-      return {
-        cards: updatedCards,
-        editeeId: "-1",
-      };
+    }
+    setCards(updatedCards);
+    setEditeeId(null);
+  }
+
+  function handleEndEditAndAdd(cardId, name, status, content, category) {
+    const cardIdNum = parseInt(cardId);
+    const updatedCards = [];
+    updatedCards.push({
+      id: cardIdNum,
+      name: name,
+      status: status,
+      content: content,
+      category: category,
+      author: props.username,
     });
-  }
-
-  handleBeginEdit(cardId) {
-    this.setState({ editeeId: cardId });
-  }
-
-  handleEndEdit(cardId) {
-    // remove card from state if it doesn't exist (a cancelled add card).
-    // remove card from editee session.
-    this.setState((state, props) => {
-      const updatedCards = [];
-      for (let card of state.cards) {
-        // id of -1 means the card didn't exist.
-        if (!(card.id === -1)) {
-          updatedCards.push({
-            id: card.id,
-            name: card.name,
-            status: card.status,
-            content: card.content,
-            category: card.category,
-            author: card.author,
-          });
-        }
+    for (let card of cards) {
+      if (!(card.id === -1)) {
+        updatedCards.push({
+          id: card.id,
+          name: card.name,
+          status: card.status,
+          content: card.content,
+          category: card.category,
+          author: card.author,
+        });
       }
-      return {
-        cards: updatedCards,
-        editeeId: null,
-      };
-    });
+    }
+    setCards(updatedCards);
+    setEditeeId(null);
   }
 
-  handleEndEditAndAdd(cardId, name, status, content, category) {
-    this.setState((state, props) => {
-      const cardIdNum = parseInt(cardId);
-      const updatedCards = [];
-      updatedCards.push({
-        id: cardIdNum,
-        name: name,
-        status: status,
-        content: content,
-        category: category,
-        author: props.username,
-      });
-      for (let card of state.cards) {
-        if (!(card.id === -1)) {
-          updatedCards.push({
-            id: card.id,
-            name: card.name,
-            status: card.status,
-            content: card.content,
-            category: card.category,
-            author: card.author,
-          });
-        }
+  function handleEndEditAndUpdate(cardId, name, status, content, category) {
+    const cardIdNum = parseInt(cardId);
+    const updatedCards = [];
+    for (let card of cards) {
+      if (card.id === cardIdNum) {
+        updatedCards.push({
+          id: card.id,
+          name: name,
+          status: status,
+          content: content,
+          category: category,
+          author: card.author,
+        });
+      } else {
+        updatedCards.push({
+          id: card.id,
+          name: card.name,
+          status: card.status,
+          content: card.content,
+          category: card.category,
+          author: card.author,
+        });
       }
-      return {
-        cards: updatedCards,
-        editeeId: null,
-      };
-    });
+    }
+    setCards(updatedCards);
+    setEditeeId(null);
   }
 
-  handleEndEditAndUpdate(cardId, name, status, content, category) {
-    this.setState((state, props) => {
-      const cardIdNum = parseInt(cardId);
-      const updatedCards = [];
-      for (let card of state.cards) {
-        if (card.id === cardIdNum) {
-          updatedCards.push({
-            id: card.id,
-            name: name,
-            status: status,
-            content: content,
-            category: category,
-            author: card.author,
-          });
-        } else {
-          updatedCards.push({
-            id: card.id,
-            name: card.name,
-            status: card.status,
-            content: card.content,
-            category: card.category,
-            author: card.author,
-          });
-        }
-      }
-      return {
-        cards: updatedCards,
-        editeeId: null,
-      };
-    });
-  }
-
-  handleDelete(cardId) {
-    postAuthJson("/api/card/delete", this.props.jwtToken, {
+  function handleDelete(cardId) {
+    postAuthJson("/api/card/delete", props.jwtToken, {
       id: cardId,
     }).then((response) => {
       if (response.ok) {
         // remove card from state
-        this.setState((state, props) => {
-          const cardIdNum = parseInt(cardId);
-          const updatedCards = [];
-          for (let card of state.cards) {
-            if (!(card.id === cardIdNum)) {
-              updatedCards.push({
-                id: card.id,
-                name: card.name,
-                status: card.status,
-                content: card.content,
-                category: card.category,
-                author: card.author,
-              });
-            }
+        const cardIdNum = parseInt(cardId);
+        const updatedCards = [];
+        for (let card of cards) {
+          if (!(card.id === cardIdNum)) {
+            updatedCards.push({
+              id: card.id,
+              name: card.name,
+              status: card.status,
+              content: card.content,
+              category: card.category,
+              author: card.author,
+            });
           }
-          return { cards: updatedCards };
-        });
+        }
+        setCards(updatedCards);
       }
     });
   }
 
-  buildCardListItem(id, name, status, content, category, author) {
-    return id === this.state.editeeId ? (
+  function buildCardListItem(id, name, status, content, category, author) {
+    return id === editeeId ? (
       <li>
         <EditeeCard
           key={id}
@@ -198,10 +166,10 @@ class BlogView extends React.Component {
           content={content}
           category={category}
           author={author}
-          jwtToken={this.props.jwtToken}
-          onEndEdit={this.handleEndEdit}
-          onEndEditAndUpdate={this.handleEndEditAndUpdate}
-          onEndEditAndAdd={this.handleEndEditAndAdd}
+          jwtToken={props.jwtToken}
+          onEndEdit={handleEndEdit}
+          onEndEditAndUpdate={handleEndEditAndUpdate}
+          onEndEditAndAdd={handleEndEditAndAdd}
         />
       </li>
     ) : (
@@ -214,37 +182,35 @@ class BlogView extends React.Component {
           content={content}
           category={category}
           author={author}
-          signedInUsername={this.props.username}
-          onEdit={this.handleBeginEdit}
-          onDelete={this.handleDelete}
+          signedInUsername={props.username}
+          onEdit={handleBeginEdit}
+          onDelete={handleDelete}
         />
       </li>
     );
   }
 
-  render() {
-    return (
-      <div className="BlogView MainView">
-        <ul className="CardList">
-          {!(this.props.username === null) && (
-            <li>
-              <NewCard key="new" onNewCard={this.handleNewCard} />
-            </li>
-          )}
-          {this.state.cards.map((card) =>
-            this.buildCardListItem(
-              card.id.toString(),
-              card.name,
-              card.status,
-              card.content,
-              card.category,
-              card.author
-            )
-          )}
-        </ul>
-      </div>
-    );
-  }
+  return (
+    <div className="BlogView MainView">
+      <ul className="CardList">
+        {props.username !== null && (
+          <li>
+            <NewCard key="new" onNewCard={handleNewCard} />
+          </li>
+        )}
+        {cards.map((card) =>
+          buildCardListItem(
+            card.id.toString(),
+            card.name,
+            card.status,
+            card.content,
+            card.category,
+            card.author
+          )
+        )}
+      </ul>
+    </div>
+  );
 }
 
 export default BlogView;
