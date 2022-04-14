21b74a9983ca39891e9587e8d193e82c3bbd4e8e
pbatchis <102021513+pbatchis@users.noreply.github.com>
2022-04-14 16:16:13 +0700

    Refactor App to a function component.

--- src/main/js/App.js
+++ src/main/js/App.js
@@ -1,106 +1,81 @@
-const React = require("react");
-const AnonControlBar = require("./AnonControlBar").default;
-const SignedInControlBar = require("./SignedInControlBar").default;
-const BlogView = require("./BlogView").default;
-const RegisterView = require("./RegisterView").default;
-const RegisterSuccessView = require("./RegisterSuccessView").default;
-const SignInView = require("./SignInView").default;
+import React, { useState } from "react";
+import AnonControlBar from "./AnonControlBar";
+import SignedInControlBar from "./SignedInControlBar";
+import BlogView from "./BlogView";
+import RegisterView from "./RegisterView";
+import RegisterSuccessView from "./RegisterSuccessView";
+import SignInView from "./SignInView";
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      viewMode: "BlogView",
-      username: null,
-      jwtToken: null,
-    };
-    this.handleBlog = this.handleBlog.bind(this);
-    this.handleRegister = this.handleRegister.bind(this);
-    this.handleRegisterSuccess = this.handleRegisterSuccess.bind(this);
-    this.handleSignIn = this.handleSignIn.bind(this);
-    this.handleSignInSuccess = this.handleSignInSuccess.bind(this);
-    this.handleSignOut = this.handleSignOut.bind(this);
-  }
+function App(props) {
+  const [viewMode, setViewMode] = useState("BlogView");
+  const [username, setUsername] = useState(null);
+  const [jwtToken, setJwtToken] = useState(null);
 
-  handleBlog(event) {
+  function handleBlog(event) {
     event.preventDefault();
-    this.setState({ viewMode: "BlogView" });
+    setViewMode("BlogView");
   }
 
-  handleRegister(event) {
+  function handleRegister(event) {
     event.preventDefault();
-    this.setState({ viewMode: "RegisterView" });
+    setViewMode("RegisterView");
   }
 
-  handleRegisterSuccess() {
-    this.setState({ viewMode: "RegisterSuccessView" });
+  function handleRegisterSuccess() {
+    setViewMode("RegisterSuccessView");
   }
 
-  handleSignIn(event) {
+  function handleSignIn(event) {
     event.preventDefault();
-    this.setState({ viewMode: "SignInView" });
+    setViewMode("SignInView");
   }
 
-  handleSignInSuccess(username, jwtToken) {
-    this.setState({
-      viewMode: "BlogView",
-      username: username,
-      jwtToken: jwtToken,
-    });
+  function handleSignInSuccess(username, jwtToken) {
+    setViewMode("BlogView");
+    setUsername(username);
+    setJwtToken(jwtToken);
   }
 
-  handleSignOut(event) {
+  function handleSignOut(event) {
     event.preventDefault();
-    this.setState({
-      viewMode: "BlogView",
-      username: null,
-      jwtToken: null,
-    });
+    setViewMode("BlogView");
+    setUsername(null);
+    setJwtToken(null);
   }
 
-  isSignedIn() {
-    return this.state.jwtToken != null;
+  function isSignedIn() {
+    return jwtToken !== null;
   }
 
-  buildMainView() {
-    switch (this.state.viewMode) {
+  function buildMainView() {
+    switch (viewMode) {
       case "BlogView":
-        return (
-          <BlogView
-            username={this.state.username}
-            jwtToken={this.state.jwtToken}
-          />
-        );
+        return <BlogView username={username} jwtToken={jwtToken} />;
       case "RegisterView":
-        return <RegisterView onRegisterSuccess={this.handleRegisterSuccess} />;
+        return <RegisterView onRegisterSuccess={handleRegisterSuccess} />;
       case "RegisterSuccessView":
-        return <RegisterSuccessView onSignIn={this.handleSignIn} />;
+        return <RegisterSuccessView onSignIn={handleSignIn} />;
       case "SignInView":
-        return <SignInView onSignInSuccess={this.handleSignInSuccess} />;
+        return <SignInView onSignInSuccess={handleSignInSuccess} />;
       default:
         return null;
     }
   }
 
-  render() {
-    return (
-      <div className="App">
-        {this.isSignedIn() ? (
-          <SignedInControlBar
-            onBlog={this.handleBlog}
-            onSignOut={this.handleSignOut}
-          />
-        ) : (
-          <AnonControlBar
-            onBlog={this.handleBlog}
-            onRegister={this.handleRegister}
-            onSignIn={this.handleSignIn}
-          />
-        )}
-        {this.buildMainView()}
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      {isSignedIn() ? (
+        <SignedInControlBar onBlog={handleBlog} onSignOut={handleSignOut} />
+      ) : (
+        <AnonControlBar
+          onBlog={handleBlog}
+          onRegister={handleRegister}
+          onSignIn={handleSignIn}
+        />
+      )}
+      {buildMainView()}
+    </div>
+  );
 }
 
 export default App;
